fa9a61146bf912ed123485a2ce5525207a462767
Ponnuri Yeswanth <yeswanthntr5000@gmail.com>
2020-05-16 11:27:09 +0530

    Create database.php

---
+++ database.php
@@ -0,0 +1,11 @@
+mysql' => array(
+        'driver'    => 'mysql',
+        'host'      => '127.0.0.1',
+        'port'      => '8889',
+        'database'  => 'databaseName',
+        'username'  => 'root',
+        'password'  => 'root',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ),
